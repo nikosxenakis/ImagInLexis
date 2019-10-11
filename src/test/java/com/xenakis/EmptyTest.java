3ee633221bdc1c713e11a396a5284024293281fc
Nikolaos Xenakis <nikosxenakis94@gmail.com>
2019-10-11 22:36:37 +0100

    maven updated

---
+++ src/test/java/com/xenakis/EmptyTest.java
@@ -0,0 +1,13 @@
+package com.xenakis;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class EmptyTest {
+
+    @Test
+    public void testLucky() {
+        assertEquals(7, 7);
+    }
+
+}
