7166b95480c31857b5bf4dda84b3c6daf763381e
Nikos Xenakis <nikosxenakis94@gmail.com>
2019-10-23 21:26:43 +0100

    SoundHolder improvements

--- src/main/java/com/xenakis/application/SoundHolder.java
+++ src/main/java/com/xenakis/application/SoundHolder.java
@@ -4,57 +4,64 @@ import com.xenakis.ImagInLexis;
 import com.xenakis.service.DatabaseUtil;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
+import org.apache.log4j.Logger;
 
 import java.net.URL;
 import java.util.HashMap;
 
 //TODO remove this with bellow class
-public class SoundHolder{
+public class SoundHolder {
 
-    private static final SoundHolder instance = new SoundHolder();
-
-    private final HashMap<String, MediaPlayer> sounds;
-    private final HashMap<String, String> soundsPath;
-
-    private SoundHolder(){
-    	this.sounds = new HashMap<>();
-    	this.soundsPath = new HashMap<>();
-    }
-
-    public static SoundHolder getInstance(){
-        return instance;
-    }
+    private final static HashMap<String, MediaPlayer> sounds = new HashMap<>();
+    private final static HashMap<String, String> soundsPath = new HashMap<>();
+    private final static Logger logger = Logger.getLogger(SoundHolder.class);
 
     private static MediaPlayer getSound(String id){
 
-        String path = instance.soundsPath.get(id);
-        System.out.println(path);
+        String path = SoundHolder.soundsPath.get(id);
+        if(path == null)
+            throw new NullPointerException("There is no sound with id = " + id);
+
+
+        SoundHolder.logger.info("getSound: id = " + id + ", path = " + path);
+
+//        System.out.println(path);
         URL resource = ImagInLexis.class.getResource(path);
         Media media = new Media(resource.toString());
         MediaPlayer mediaPlayer = new MediaPlayer(media);
-        instance.sounds.put(id, mediaPlayer);
+        SoundHolder.sounds.put(id, mediaPlayer);
 
         return mediaPlayer;
     }
 
-    public static void playSound(String id){
-        getSound(id).play();
+    public static void playSound(String id) {
+        MediaPlayer mediaPlayer;
+
+        try {
+            mediaPlayer = getSound(id);
+        }
+        catch (NullPointerException ex) {
+            SoundHolder.logger.error(ex.getMessage());
+            return;
+        }
+
+        mediaPlayer.play();
     }
 
     public static void stopSound(String id){
 
-    	MediaPlayer mediaPlayer = instance.sounds.get(id);
+    	MediaPlayer mediaPlayer = SoundHolder.sounds.get(id);
     	if(mediaPlayer != null){
     		mediaPlayer.stop();
-    		instance.sounds.remove(id);
+            SoundHolder.sounds.remove(id);
     	}
     }
 
     public static void addSound(String id, String path){
-    	instance.add(id, path);
+        SoundHolder.add(id, path);
     }
 
-    private void add(String id, String path){
+    private static void add(String id, String path){
     	soundsPath.put(id, path);
     }
 }
