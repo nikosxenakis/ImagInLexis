ecb40bc41af4d41cc44e07d8022c734fd8b20a12
Nikos Xenakis <nikosxenakis94@gmail.com>
2019-10-27 18:50:33 +0000

    minor improvements

--- src/main/resources/com/xenakis/database/create_tables.sql
+++ src/main/resources/com/xenakis/database/create_tables.sql
@@ -24,23 +24,6 @@ CREATE TABLE IF NOT EXISTS "categories" (
 	FOREIGN KEY("chapterId") REFERENCES "categories"
 );
 
-CREATE TABLE IF NOT EXISTS "scores" (
-	"id"	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
-	"username"	TEXT,
-	"time"	TEXT,
-	"date"	TEXT,
-	"score"	INTEGER,
-	"chapterId"	INTEGER,
-	"categoryId" INTEGER,
-	FOREIGN KEY("chapterId") REFERENCES "chapters"("id"),
-	FOREIGN KEY("categoryId") REFERENCES "categories"("id")
-);
-
-CREATE TABLE IF NOT EXISTS "questions" (
-	"id"	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
-	"name"	TEXT NOT NULL
-);
-
 CREATE TABLE IF NOT EXISTS "screenTypes" (
 	"id"	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
 	"name"	TEXT NOT NULL
@@ -59,3 +42,48 @@ CREATE TABLE IF NOT EXISTS "users" (
 	"name"	TEXT NOT NULL,
 	"active"	INTEGER NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS "scores" (
+	"id"	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+	"username"	TEXT,
+	"time"	TEXT,
+	"date"	TEXT,
+	"score"	INTEGER,
+	"chapterId"	INTEGER,
+	"categoryId" INTEGER,
+	FOREIGN KEY("chapterId") REFERENCES "chapters"("id"),
+	FOREIGN KEY("categoryId") REFERENCES "categories"("id")
+);
+
+-- CREATE TABLE IF NOT EXISTS "questions" (
+-- 	"id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+-- 	"chapterId" INTEGER NOT NULL,
+-- 	"categoryId" INTEGER NOT NULL,
+-- 	"screenTypeId" INTEGER NOT NULL,
+-- 	"name" TEXT NOT NULL,
+-- 	"question" TEXT NOT NULL,
+
+-- 	-- nullable
+-- 	"basicImageId" INTEGER,
+-- 	"image1Id" INTEGER,
+-- 	"image2Id" INTEGER,
+-- 	"image3Id" INTEGER,
+-- 	"soundId" INTEGER,
+
+-- 	FOREIGN KEY("chapterId") REFERENCES "chapters"("id"),
+-- 	FOREIGN KEY("categoryId") REFERENCES "categories"("id"),
+-- 	FOREIGN KEY("screenTypeId") REFERENCES "screenTypes"("id"),
+-- 	FOREIGN KEY("basicImageId") REFERENCES "images"("id"),
+-- 	FOREIGN KEY("image1Id") REFERENCES "images"("id"),
+-- 	FOREIGN KEY("image2Id") REFERENCES "images"("id"),
+-- 	FOREIGN KEY("image3Id") REFERENCES "images"("id"),
+-- 	FOREIGN KEY("soundId") REFERENCES "sounds"("id")
+-- );
+
+
+-- CREATE TABLE IF NOT EXISTS "questionAnswers" (
+-- 	"id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+-- 	"questionId" INTEGER NOT NULL,
+-- 	"answer" INTEGER NOT NULL,
+-- 	FOREIGN KEY("questionId") REFERENCES "questions"("id")
+-- );
